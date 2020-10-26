85af78049e4d846cd2723a2640b1fab2f76248f3
zhangxing <1017073490@qq.com>
2020-10-26 15:03:57 +0800

    添加文件

---
+++ Git1.0/src/main/java/Hello/git.java
@@ -0,0 +1,12 @@
+package Hello;
+
+/**
+ * @author zhangxing
+ * @Description: Test
+ * @date 2020/10/26 14:55
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("Git学习。。。测试");
+    }
+}
